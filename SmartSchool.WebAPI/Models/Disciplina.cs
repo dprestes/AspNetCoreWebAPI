adc38faa3e59715fc802c8aff4f996c9e5219978
douglasmendes94 <douglas.mendes@woli.tec.br>
2022-04-26 01:15:54 -0300

    Adding my course on GitHub

---
+++ SmartSchool.WebAPI/Models/Disciplina.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+
+namespace SmartSchool.WebAPI.Models
+{
+    public class Disciplina
+    {
+        public Disciplina() { }
+
+        public Disciplina(int id, string nome, int professorId)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.professorId = professorId;
+        }
+        public int id { get; set; }
+        public string nome { get; set; }
+        public int professorId { get; set; }
+        public Professor Professor { get; set; }
+
+        public IEnumerable<AlunoDisciplina> AlunosDisciplinas { get; set; }
+    }
+}
